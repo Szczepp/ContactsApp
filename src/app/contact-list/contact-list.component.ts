41f81c32a863ac33b99b5d674e6a2ca37fd0edb1
Szczepan <szczep.kowalik@gmail.com>
2022-11-03 23:42:53 +0100

    Created contact-list and contact components. UserService contains method with request to ContactsAppAPI.

---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, Output } from "@angular/core";
+import { UserService } from "../user.service";
+
+export interface Contact {
+    id: number,
+    name: string,
+    email: string, 
+    phone: string,
+    password: string, 
+    category: string, 
+    dateOfBirth: Date
+}
+
+@Component({
+    selector: 'contact-list',
+    templateUrl: './contact-list.component.html',
+    styleUrls: ['./contact-list.component.css']
+})
+
+export class ContactListComponent implements OnInit{
+    contactToLoad: any;
+    contacts: Contact[] | undefined;
+    loadContactCheck = false;
+    title = 'Contacts';
+    constructor(
+       private httpClient: HttpClient,
+       private userService: UserService
+    ) {
+    }
+    ngOnInit(): void {
+        this.getContacts();
+    }
+
+    loadContact(contactId: Contact){
+        this.loadContactCheck = true;
+        this.contactToLoad = contactId;
+    }
+
+    getContacts() {
+        this.userService.getContacts().subscribe(
+            response => {
+                console.log(response);
+                this.contacts = response;
+            }
+        );
+    }
+}
---
+++ src/app/contact-list/contact/contact.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, Input, OnChanges } from "@angular/core";
+import { Contact } from "../contact-list.component";
+import { UserService } from "../../user.service";
+
+
+@Component({
+    selector: 'contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.css']
+})
+
+export class ContactComponent implements OnChanges{
+    @Input() contactToLoad : any = '';
+
+    contact!: Contact; 
+    constructor(
+       private httpClient: HttpClient,
+       private userService: UserService
+
+    ) {
+    }
+    ngOnChanges(): void {
+        this.getContact()
+    }
+    getContact() {
+        this.userService.getContact(this.contactToLoad.id).subscribe(response => {
+            console.log(response);
+            this.contact = response;
+        });
+
+    }
+}
